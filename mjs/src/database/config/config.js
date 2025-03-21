fc7969b375c4ebdd2b642c6aa312ef2260d6ddad
Santiago Jimenez <santiagojn15@gmail.com>
2025-03-21 19:15:25 -0300

    cree base de datos y lo conecte con sql

--- mjs/src/database/config/config.js
+++ mjs/src/database/config/config.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
+
 module.exports = {
   development: {
-    username: "root",
-    password: "root",
-    database: "database_development",
-    host: "127.0.0.1",
+  username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql"
   },
   test: {
@@ -13,7 +16,7 @@ module.exports = {
     host: "127.0.0.1",
     dialect: "mysql"
   },
-  production: {
+  production : {
     username: "root",
     password: null,
     database: "database_production",
